ed3ea9ab8bc631218ac22b7a051bb6dc2dc44f57
harisparavoor <harisparavoor@gmail.com>
2026-07-09 13:18:58 +0530

    dev.prod

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mystatetestfile"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "mystatetestfile"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-1"
     #profile = "harips"
     encrypt = true
     #dynamodb_table = "terraform-lock-table"
